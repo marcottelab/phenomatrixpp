426bbce2688a072ed796870ab8d01a696f129f48
John Woods <john.woods@marcottelab.org>
2010-01-15 18:34:36 -0600

    Shuffled version.cpp contents to version.h, need to find a way to include VERSION and ARCH

--- src/version.h
+++ src/version.h
@@ -2,6 +2,17 @@
 # define _VERSION_H_
 
 # include <string>
-void print_program_header();
+
+const std::string VERSION  = "@VERSION@";
+const std::string PLATFORM = "@ARCH@";
+
+// Prints program version information.
+void print_program_header() {
+    cout << "Phenomatrix++ v" << VERSION << " on " << PLATFORM << endl;
+    cout << "Copyright John O. Woods, 2009 - 2010" << endl;
+    cout << "The Marcotte Lab, The University of Texas at Austin" << endl;
+    cout << endl;
+}
+
 #endif
 
